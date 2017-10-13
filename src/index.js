a1282060c6060e58c75e5f76e89f306c9eb703f4
Jung Hyun Kim <siurilove@jungs-mbp.attlocal.net>
2017-10-12 21:56:54 -0500

    User Fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './index.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+import {getUsers, deleteUser} from './api/userApi';
+
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser" >Delete</a> </td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lasstName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});

fd72de57cf6887d8f27362c4195c7c1fef34bdca
Jung Hyun Kim <siurilove@jungs-mbp.attlocal.net>
2017-10-13 22:45:17 -0500

    Making HTTP Call

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,28 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
 
+const baseUrl = getBaseUrl();
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+//can't call func delete s
 export function getUsers() {
   return get('users');
 }
 
-function get(url) {
-  return fetch(url).then(onSuccess, onError);
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
